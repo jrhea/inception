49f15db083dff4b6d3cc757feecdea85b88613bc
jonny rhea <jonathan.rhea@gmail.com>
2018-02-14 08:23:56 -0600

    removed extra dependencies. switched from requirejs.  made index.html a form to paste code in order to host content.

--- js/main.js
+++ js/main.js
@@ -1,109 +1,80 @@
-requirejs.config({
-    baseUrl: 'js/lib', 
-	paths: {
-		'pako': 'pako.min',
-		'jquery':'jquery.min',
-		'text':'text',
-		'validate':'validate.min'
-	}
-    ,
-	config: {
-		text: {
-			useXhr: function (url, protocol, hostname, port){
-				// allow cross-domain requests
-				return true;
-			}			
-		}
-	}
-});
-require([
-	'pako',
-	'jquery',
-	'validate'
-], 
-function(pako,jquery,validate) {
-    "use strict";
+"use strict";
+var inceptionHost = inceptionHost || {};
+inceptionHost.sub = (function() {
     var command,target,resource;
-	var url = new URL(location);
-	
-	command = url.searchParams.get("command");
-	target = url.searchParams.get("target");
-	resource = url.searchParams.get("resource");
+    var url = new URL(location);
+    
+    command = url.searchParams.get("command");
+    target = url.searchParams.get("target");
+    resource = getHashPayload(location);
+    
+    if(!resource){
+        init();
+    }
+    else
+    {
+        if(!command){
+            command = "create";
+            target = "hyperlink";
+        }
+        main();
+    }
 
-	//detect if called from content in iframe's srcdoc
-	if(url.pathname === "srcdoc"){
-		//TODO: make a more sophisticated method for detecting iframe events
-		resource = getHashPayload(location);
-		//event was fired from within iframe
-		if(resource){
-			//if we are in an iframe, set url back to parent url so that api can be called
-			url = new URL(parent.location.origin + parent.location.pathname);
-			//TODO: make these dynamic and defined by the source that is being loaded
-			command="create";
-			target="newWindow";
-		}
-	}
-	
-	if(command || target || resource){
-		main();
-	}
+    function init(){
+        var quill = new Quill('#editor-container', {
+          modules: {
+            syntax: true,
+            toolbar: false
+          },
+          placeholder: 'type or paste html code here...'
+          //theme: 'bubble'  // or 'snow'
+        });
+        document.getElementsByClassName("ql-editor")[0].innerHTML="<pre class=\"ql-syntax\" spellcheck=\"false\"></pre>"
 
+        function onPublish() {
+            var myLink = document.getElementById('mylink');
+            var html = quill.getText();
+            myLink.onclick = function(){
+                var script = document.createElement("script");
+                script.type = "text/javascript";
+                script.src = "./js/main.js"; 
+                document.getElementsByTagName("head")[0].appendChild(script);
+                document.location.hash=serialize(html.replace(/"/g, "'"));
+                return false;
+            }
+            document.getElementById('mylink').click(); 
+        }
+        document.getElementById("btnPublish").addEventListener("click", onPublish);
+
+    }
 	function main(){
 		if(command === "create"){
-			
-			if(isUri(resource)){
-				
-				// $.ajax({
-					// url: resource,
-					// dataType: 'html'
-				// }).done(function(html) {
-					// createHyperlink(html);
-				// });
-				
-				require(['text!' + resource],
-					function(text){
-						createHyperlink(text);
-					},
-					function(err){
-						console.log(err);
-					}
-				);
-				
-			}
-			else{
-				var element = eval(resource);
-				if(element){
-					createHyperlink(element.innerHTML);
-				}
-				else{
-					console.log(resource + " is not in the DOM.")
-				}
-			}
+			createHyperlink(resource);
 		}
 		else if(command === "load"){
 			loadHyperlink();
 		}
 	}
 	
-	function createHyperlink(html){
+	function createHyperlink(){
 		//change the command to load so the hyperlink instructs the api to load the resource
 		command = "load";
 		url.searchParams.set("command",command);
 		url.searchParams.delete("source");
 		url.searchParams.delete("target");
 		url.searchParams.delete("resource");
-		var hyperlink = url.href + "#?" + serialize(html.replace(/"/g, "'"));
+        url.hash = "";
+		var hyperlink = url.href + "#?" + resource;
 		if(target === 'newWindow'){
 			document.location=hyperlink;
 		}
 		else if(target === 'hyperlink'){
-			document.body.innerHTML += "<a href= " + hyperlink + ">" + "Inception. it Is possible" + "</a>";
+			document.body.innerHTML = "<a href= " + hyperlink + ">" + "Inception. It is possible." + "</a>";
 		}
 	}
 		
 	function loadHyperlink(){
-		var payload = getHashPayload(location);
-		var deserializedPayload = deserialize(payload);
+		var deserializedPayload = deserialize(resource);
 		var iframe = "<iframe id=\"contentFrame\" srcdoc=\"" + deserializedPayload + "\" style='position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;'><p>Your browser does not support iframes.</p></iframe>";
 		document.body.innerHTML=iframe;
 
@@ -135,20 +106,5 @@ function(pako,jquery,validate) {
     function deserialize(payload) {
         return JSON.parse( pako.inflate( atob(payload), { to: 'string' }));
     }
-	
-	function isUri(resource){
-		var result = true;
-		var object = validate({website: resource},{
-			website: {
-				url: {
-					allowLocal: true
-				}
-			}
-		});
-		//object undefined means that the resource is a valid uri
-		if(object){
-			result = false
-		}
-		return result;
-	}
-});
+    
+})();
